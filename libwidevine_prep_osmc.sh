0f77cf9e0f0d3f750df3b733218f80f7b4a05ec8
Joakim Sandström <joakim.s@webbkontakt.net>
2022-09-20 16:44:16 +0200

    Update libwidevine_prep_osmc.sh
    Removed pre patched libc detection and support, wll add a check if patched libc is install iin buster, else do it. Still need to test  if there are changes in python3.

--- libwidevine_prep_osmc.sh
+++ libwidevine_prep_osmc.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-debver=$(echo < /etc/debian_version)
-if [[ $debver -lt 11 ]]
-then
-  mkdir ~/temp_glibc
-  cd ~/temp_glibc
-  wget -r --user="anonymous" --password="guest@email.com" ftp://ftp.acc.umu.se/mirror/osmc.tv/osmc/apt/pool/main/g/glibc/*_armhf.deb -m
-  cd ~/temp_glibc/ftp.acc.umu.se/mirror/osmc.tv/osmc/apt/pool/main/g/glibc
-  sudo dpkg -i *.deb
-  sudo apt-get install -f
-  rm -rf ~/temp_glibc  
-fi
+#debver=$(echo < /etc/debian_version)
+#if [[ $debver -lt 11 ]]
+#then
+#  mkdir ~/temp_glibc
+#  cd ~/temp_glibc
+#  wget -r --user="anonymous" --password="guest@email.com" ftp://ftp.acc.umu.se/mirror/osmc.tv/osmc/apt/pool/main/g/glibc/*_armhf.deb -m
+#  cd ~/temp_glibc/ftp.acc.umu.se/mirror/osmc.tv/osmc/apt/pool/main/g/glibc
+#  sudo dpkg -i *.deb
+#  sudo apt-get install -f
+#  rm -rf ~/temp_glibc  
+#fi
 # Map parameters to coder-friendly names.
-Program="kodi"
+program="kodi"
 version="19"
-# thanks DavorPerkovac for TypeO in above varible namne
-
-# Program name must be a valid command.
-command -v $Program >/dev/null 2>&1 || { echo "Command: $Program not found."; exit 99; }
-InstalledVersion=$( "$Program" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/' )
+command -v $program >/dev/null 2>&1 || { echo "Command: $program not found."; exit 99; }
+InstalledVersion=$( "$program" --version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/' )
 NonDecIV=${InstalledVersion%.*}
 if [[ $NonDecIV -lt $version ]]; then
    if [ ! -d /usr/share/doc/python-crypto ]; then
